a5be24e8918e4895b81e90fe85223334ba3aa225
선우 <ksw-12345@naver.com>
2023-05-06 15:59:02 +0900

    코딩애플 next.js1-5강
    20230506

--- next jangin/fresh/app/list/page.js
+++ next jangin/fresh/app/list/page.js
@@ -1,13 +1,16 @@
 export default function List() {
+  let 상품 = ["Tomatoes", "Pasta", "Coconuts"];
+
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      <div>
-        <h4 className="food">상품1 $40</h4>
-      </div>
-      <div>
-        <h4 className="food">상품2 $40</h4>
-      </div>
+      {상품.map((a, i) => {
+        return (
+          <div>
+            <h4 className="food">{상품[i]} $40</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
