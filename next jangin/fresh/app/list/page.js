26c4cbce5ea905cf0e8ef31b0a73dd814a5a004c
선우 <ksw-12345@naver.com>
2023-05-06 16:51:32 +0900

    230506
    찐6강 전에거는 5강이었던거임
    윤재국 이 부장놈

--- next jangin/fresh/app/list/page.js
+++ next jangin/fresh/app/list/page.js
@@ -6,8 +6,10 @@ export default function List() {
       <h4 className="title">상품목록</h4>
       {상품.map((a, i) => {
         return (
-          <div>
-            <h4 className="food">{상품[i]} $40</h4>
+          <div className="food" key={i}>
+            <img src={"food" + i + ".png"} className="food-img" />
+            {/* {`/food${i}.png`}로도 나타낼 수 있다. */}
+            <h4>{a} $40</h4>
           </div>
         );
       })}
